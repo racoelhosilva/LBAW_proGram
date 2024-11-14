bc2a7c154a1353f0e2a1da142c76f14ef9556c8c
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-11-14 22:30:53 +0000

    Added user stats

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 // Added to define Eloquent relationships.
@@ -47,6 +48,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function stats(): HasOne
+    {
+        return $this->hasOne(UserStats::class);
+    }
 
     /**
      * Get the cards for a user.
---
+++ app/Models/UserStats.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserStats extends Model
+{
+    use HasFactory;
+
+    protected $table = 'user_stats';
+
+    protected $fillable = [
+        'github_url',
+        'gitlab_url',
+        'linkedin_url',
+    ];
+}
