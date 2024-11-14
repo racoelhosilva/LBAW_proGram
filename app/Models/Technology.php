c8fcb5b23beb32e373e43e93b2bb917292fb9c20
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-11-14 22:48:09 +0000

    Added technology model

---
+++ app/Models/Technology.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Technology extends Model
+{
+    use HasFactory;
+
+    protected $table = 'technology';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+    ];
+}
