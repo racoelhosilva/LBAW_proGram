4a25028cdc4e443477224c8def822946111f09fb
Process-ing <rbruno.p.o@gmail.com>
2024-11-14 23:16:04 +0000

    Create home template

---
+++ resources/views/auth/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div>
+        <h1>Users You Might Nnow</h1>
+        <p>No users at the moment</p>
+    </div>
+    <div>
+        <h1>Recommended Posts</h1>
+        <p>No posts at the moment</p>
+    </div>
+    <div>
+        <h1>Trending Tags</h1>
+        <p>No tags at the moment</p>
+    </div>
+@endsection
