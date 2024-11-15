6d4163f1b8405d2d0e7703afe43902b1042eb1cf
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-11-15 00:15:44 +0000

    feat: function to retrieve all likes of a comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
@@ -42,5 +43,10 @@ class Comment extends Model
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function allLikes(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'post_like', 'post_id', 'liker_id')->withPivot('timestamp');
     }
 }
