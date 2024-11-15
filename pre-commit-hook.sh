8d059ed016de501ad494bbf0812e0f9190bc77fd
Process-ing <rbruno.p.o@gmail.com>
2024-11-15 16:02:21 +0000

    test

--- pre-commit-hook.sh
+++ pre-commit-hook.sh
@@ -2,13 +2,23 @@ mkdir -p .git/hooks
 tee .git/hooks/pre-commit << EOF
 #!/bin/bash
 
-# IDE Helper
-php artisan ide-helper:generate
-php artisan ide-helper:eloquent
-php artisan ide-helper:models -N
+function ide_helper() {
+    php artisan ide-helper:generate 
+    php artisan ide-helper:eloquent
+    php artisan ide-helper:models -N
+}
 
-# PHP Lint
-./vendor/bin/pint --repair
+function laravel_pint() {
+    ./vendor/bin/pint --repair
+}
+
+if ! ide_helper; then
+    echo "IDE Helper files regenerated or an error occurred" >&2
+    exit 1
+elif ! laravel_pint; then
+    echo "Laravel Pint reformatted files" >&2
+    exit 1
+fi
 EOF
 
 chmod +x .git/hooks/pre-commit
