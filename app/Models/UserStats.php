94aa1d934d0ed2ce60364167eba7e354af19041f
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-11-15 00:40:30 +0000

    feat (user_stats): function to retrieve technologies and languages

--- app/Models/UserStats.php
+++ app/Models/UserStats.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Model;
 
 class UserStats extends Model
@@ -21,5 +22,15 @@ class UserStats extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class, 'user_stats_technology', 'technology_id', 'user_stats_id');
+    }
+
+    public function languages(): BelongsToMany
+    {
+        return $this->belongsToMany(Language::class, 'user_stats_language', 'language_id', 'user_stats_id');
     }
 }
