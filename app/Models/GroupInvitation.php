5ed408a5c2e7104f7e2ed96c8b1bcdb485afe26b
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-11-15 00:05:10 +0000

    feat: group invitation model

---
+++ app/Models/GroupInvitation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Model;
+
+class GroupInvitation extends Model
+{
+    use HasFactory;
+
+    protected $table = 'group_invitation';
+
+    protected $fillable = [
+        'creation_timestamp',
+        'status',
+    ];
+
+    protected $casts = [
+        'creation_timestamp' => 'datetime',
+    ];
+
+    public function group(): BelongsTo
+    {
+        return $this->belongsTo(Group::class, 'group_id');
+    }
+
+    public function invitee(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'invitee_id');
+    }
+}
