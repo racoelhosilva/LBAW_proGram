5785761fd79fc75973724d1fa72e50fe2b059348
Process-ing <rbruno.p.o@gmail.com>
2024-11-17 20:09:56 +0000

    Improve search field outline

--- resources/views/partials/search-field.blade.php
+++ resources/views/partials/search-field.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ url('/') }}" method="get" class="h-12 px-4 rounded-full bg-white dark:bg-slate-700 text-gray-500 dark:text-white border border-gray-300 dark:border-slate-600 focus-within:border-gray-500 focus-within:dark:border-white font-medium inline-flex shadow">
+<form action="{{ url('/') }}" method="get" class="h-12 px-4 rounded-full bg-white dark:bg-slate-700 text-gray-500 dark:text-white border border-gray-300 dark:border-slate-600 focus-within:border-gray-500 focus-within:dark:border-slate-400 font-medium inline-flex shadow">
     <button type="submit" aria-label="Search" class="pe-2">
         @include('partials.icon', ['name' => 'search'])
     </button>
