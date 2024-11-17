d9a02914dcc1d11254021a937c7292dfa2c5187c
Process-ing <rbruno.p.o@gmail.com>
2024-11-17 21:35:45 +0000

    Add image to user card

--- resources/views/partials/user-card.blade.php
+++ resources/views/partials/user-card.blade.php
@@ -1,4 +1,7 @@
-<article class="bg-slate-800 border border-slate-600 p-3 rounded-xl shadow">
+<article class="p-3 rounded-xl bg-slate-800 border border-slate-600 shadow flex items-center">
+    {{-- TODO: Use real profile picture --}}
+    {{-- TODO: Find better place for placeholder --}}
+    <img src="{{ url('img/placeholder.png') }}" alt="{{ $user->name }}" class="w-12 h-12 me-3 rounded-full object-cover">
     <div>
         <p class="text-base/4 font-medium">{{ $user->name }}</p>
         <a href="{{ url('user/' . $user->id) }}" class="text-xs/3 pt-1 font-medium text-gray-500 dark:text-gray-400">{{ '@' . $user->handle }}</a>
