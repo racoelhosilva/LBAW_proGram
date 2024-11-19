e560925277489f5826727fb2c3205bc695b0e4af
Process-ing <rbruno.p.o@gmail.com>
2024-11-19 20:06:43 +0000

    Improve header HTML

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,12 @@
-<header class="h-16 px-24 grid grid-cols-[auto_1fr_auto_auto_auto_auto] gap-2 items-center justify-end">
-    <h1>ProGram</h1>
-    <span>@include('partials.search-field')</span>  {{-- To prevent search field from expanding all available space --}}
-    @include('partials.icon-button', ['iconName' => 'home', 'label' => 'Home', 'type' => 'secondary'])
-    @include('partials.theme-button')
-    @include('partials.text-button', ['text' => 'Login/Register', 'type' => 'primary'])
-    @include('partials.icon-button', ['iconName' => 'grip', 'label' => 'More', 'type' => 'transparent'])
+<header class="h-16 px-24 grid grid-cols-[1fr_auto] items-center justify-end">
+    <div class="inline-flex gap-2 items-center">  {{-- Left elements --}}
+        <p>ProGram</p>
+        @include('partials.search-field')  {{-- To prevent search field from expanding all available space --}}
+    </div>
+    <div class="inline-flex gap-2 items-center">  {{-- Right elements --}}
+        @include('partials.icon-button', ['iconName' => 'home', 'label' => 'Home', 'type' => 'secondary'])
+        @include('partials.theme-button')
+        @include('partials.text-button', ['text' => 'Login/Register', 'type' => 'primary'])
+        @include('partials.icon-button', ['iconName' => 'grip', 'label' => 'More', 'type' => 'transparent'])
+    </div>
 </header>
