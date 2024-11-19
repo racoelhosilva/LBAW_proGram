d62b39f97c77c9693e9d668371a13617b324ca98
Process-ing <rbruno.p.o@gmail.com>
2024-11-19 23:19:46 +0000

    Fix padding on footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
         <main>
             @yield('content')
         </main>
-        <footer class="py-4">
+        <footer class="p-4">
             <p class="text-xs font-medium text-gray-500 dark:text-gray-400">© 2024 ProGram. All rights reserved.</p>
         </footer>
     </body>
