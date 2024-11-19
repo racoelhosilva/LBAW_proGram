3ab26d1bad69ec3bc32795b618e5eaa2ef231fc5
Process-ing <rbruno.p.o@gmail.com>
2024-11-19 15:13:55 +0000

    Create page header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header class="px-24 py-2 grid grid-cols-[auto_1fr_auto_auto_auto_auto] gap-2 items-center justify-end">
+    <h1>ProGram</h1>
+    <span>@include('partials.search-field')</span>
+    @include('partials.icon-button', ['iconName' => 'home', 'label' => 'Home', 'type' => 'secondary'])
+    @include('partials.icon-button', ['iconName' => 'moon', 'label' => 'Light/Dark Mode', 'type' => 'secondary'])
+    @include('partials.text-button', ['text' => 'Login/Register', 'type' => 'primary'])
+    @include('partials.icon-button', ['iconName' => 'grip', 'label' => 'More', 'type' => 'transparent'])
+</header>
