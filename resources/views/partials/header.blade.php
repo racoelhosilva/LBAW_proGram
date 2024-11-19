17c7a36b7ca2bf86ffd8aac849a9593d250f6ea6
Process-ing <rbruno.p.o@gmail.com>
2024-11-19 23:12:04 +0000

    Include logo in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,9 @@
 <header class="h-16 px-24 grid grid-cols-[1fr_auto] items-center justify-end">
-    <div class="inline-flex gap-2 items-center">  {{-- Left elements --}}
-        <p>ProGram</p>
+    <div class="inline-flex gap-8 items-center">  {{-- Left elements --}}
+        <a href="{{ route('home') }}">
+            <img src="{{ url('svg/logo-large-black.svg') }}" alt="Logo" class="h-12 dark:hidden">
+            <img src="{{ url('svg/logo-large-white.svg') }}" alt="Logo" class="h-12 hidden dark:block">
+        </a>
         @include('partials.search-field')  {{-- To prevent search field from expanding all available space --}}
     </div>
     <div class="inline-flex gap-2 items-center">  {{-- Right elements --}}
