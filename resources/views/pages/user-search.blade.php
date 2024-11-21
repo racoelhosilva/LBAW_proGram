b41037fc1d6d88aacbbacd5b1b3e0c3edb98648e
Process-ing <rbruno.p.o@gmail.com>
2024-11-21 17:50:10 +0000

    Improve user search text

--- resources/views/pages/user-search.blade.php
+++ resources/views/pages/user-search.blade.php
@@ -1,11 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <main class="p-4">
-        <section class="flex flex-col">
-            @foreach ($users as $user)
-                @include('partials.user-card', ['user' => $user])
-            @endforeach
-        </section>
+    <main class="p-4 grid grid-cols-3 gap-6">
+        @if (count($users) > 0)
+            <section class="card h-min">
+                <h1 class="text-xl font-semibold">Found {{ count($users) . (count($users) === 1 ? ' user' : ' users') }}</h1>
+            </section>
+            <section class="flex flex-col col-span-2">
+                @foreach ($users as $user)
+                    @include('partials.user-card', ['user' => $user])
+                @endforeach
+            </section>
+        @else
+            <section class="flex justify-center items-center col-span-3">
+                <p class="text-lg font-medium text-center">No users found.</p>
+            </section>
+        @endif
     </main>
 @endsection
