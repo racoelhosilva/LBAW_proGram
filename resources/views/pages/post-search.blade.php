c0bced9933e43e280f1a88da59bc8f5ea2efa62b
Process-ing <rbruno.p.o@gmail.com>
2024-11-21 19:07:16 +0000

    Refactor page layout

--- resources/views/pages/post-search.blade.php
+++ resources/views/pages/post-search.blade.php
@@ -1,21 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <main class="p-4 grid grid-cols-3 gap-6">
+    <main class="p-4 grid grid-cols-4 gap-6">
         @if (count($posts) > 0)
             <section class="card h-min">
-                <h1 class="pb-4 text-xl font-semibold">Found {{ count($posts) . (count($posts) === 1 ? ' post' : ' posts') }}</h1>
+                <h1 class="pb-2 text-xl font-semibold">Search Options</h1>
                 <div class="border-t hover:bg-slate-300 hover:dark:bg-slate-600 border-slate-300 dark:border-slate-600 transition-colors">
                     <a href="{{ route('user-search', ['query' => request('query')]) }}" class="py-2 font-medium flex">Search users</a>
                 </div>
             </section>
-            <section class="flex flex-col col-span-2 gap-3">
+            <section class="flex flex-col col-span-3 gap-3">
+                <h1 class="text-xl font-semibold">Found {{ count($posts) . (count($posts) === 1 ? ' post' : ' posts') }}</h1>
                 @foreach ($posts as $post)
                     @include('partials.post-card', ['post' => $post])
                 @endforeach
             </section>
         @else
-            <section class="flex justify-center items-center col-span-3">
+            <section class="flex justify-center items-center col-span-4">
                 <p class="text-lg font-medium text-center">No posts found.</p>
             </section>
         @endif
--- resources/views/pages/user-search.blade.php
+++ resources/views/pages/user-search.blade.php
@@ -1,21 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <main class="p-4 grid grid-cols-3 gap-6">
+    <main class="p-4 grid grid-cols-4 gap-6">
         @if (count($users) > 0)
             <section class="card h-min">
-                <h1 class="pb-4 text-xl font-semibold">Found {{ count($users) . (count($users) === 1 ? ' user' : ' users') }}</h1>
-                <div class="border-t hover:bg-slate-300 hover:dark:bg-slate-600 border-slate-300 dark:border-slate-600 transition-colors">
+                <h1 class="pb-2 text-xl font-semibold">Search Options</h1>
+                <div class="hover:bg-slate-300 hover:dark:bg-slate-600 border-slate-300 dark:border-slate-600 transition-colors">
                     <a href="{{ route('post-search', ['query' => request('query')]) }}" class="py-2 font-medium flex">Search posts</a>
                 </div>
             </section>
-            <section class="flex flex-col col-span-2 gap-3">
+            <section class="flex flex-col col-span-3 gap-3">
+                <h1 class="text-xl font-semibold">Found {{ count($users) . (count($users) === 1 ? ' user' : ' users') }}</h1>
                 @foreach ($users as $user)
                     @include('partials.user-card', ['user' => $user])
                 @endforeach
             </section>
         @else
-            <section class="flex justify-center items-center col-span-3">
+            <section class="flex justify-center items-center col-span-4">
                 <p class="text-lg font-medium text-center">No users found.</p>
             </section>
         @endif
