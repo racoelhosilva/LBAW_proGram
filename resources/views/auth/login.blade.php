bab8390f939a23488fc581e68f553b2b682809d2
Process-ing <rbruno.p.o@gmail.com>
2024-11-23 20:07:31 +0000

    Refactor login page appearance

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<main id="login-page">
+<main id="login-page" class="grid grid-cols-3">
     @include('partials.login-form', ['route' => 'login', 'logo' => 'partials.logo'])
 </main>
 @endsection
--- resources/views/partials/login-form.blade.php
+++ resources/views/partials/login-form.blade.php
@@ -1,6 +1,6 @@
 @props(['route', 'logo'])
 
-<article class="card p-10 grid gap-4 justify-items-center">
+<article class="card p-10 grid gap-4 justify-items-center col-start-2">
     @include($logo, ['size' => 'large'])
 
     <form method="post" action="{{ route('login') }}" class="grid gap-4 justify-self-stretch">
@@ -9,11 +9,11 @@
         @include('partials.input-field', ['name' => 'email', 'label' => 'E-mail', 'type' => 'email', 'value' => old('email'), 'placeholder' => 'me@password.com', 'required' => true])
         @include('partials.input-field', ['name' => 'password', 'label' => 'Password', 'type' => 'password', 'placeholder' => 'password123', 'required' => true])
 
-        <label>
-            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-        </label>
-
         <div class="flex flex-col">
+            <label class="mb-2">
+                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+            </label>
+
             @include('partials.text-button', ['text' => 'Login', 'label' => 'Login', 'type' => 'primary', 'submit' => true])
             @if (session('success'))
                 <p class="text-sm text-green-500">{{ session('success') }}</p>
