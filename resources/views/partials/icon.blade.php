0c4784581273023bec73aa7d449d8253e9cc8321
Process-ing <rbruno.p.o@gmail.com>
2024-11-24 18:08:03 +0000

    Create admin header dropdown

--- resources/views/partials/icon.blade.php
+++ resources/views/partials/icon.blade.php
@@ -43,4 +43,7 @@
     @case('user-round-plus')
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round-plus"><path d="M2 21a8 8 0 0 1 13.292-6"/><circle cx="10" cy="8" r="5"/><path d="M19 16v6"/><path d="M22 19h-6"/></svg>
         @break
+    @case('user-round-x')
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round-x"><path d="M2 21a8 8 0 0 1 11.873-7"/><circle cx="10" cy="8" r="5"/><path d="m17 17 5 5"/><path d="m22 17-5 5"/></svg>
+        @break
 @endswitch
