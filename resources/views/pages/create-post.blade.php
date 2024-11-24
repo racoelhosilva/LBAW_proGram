110bf6a229a2c83ab860011972552f3e043aa989
racoelhosilva <racoelhosilva@gmail.com>
2024-11-24 16:00:39 +0000

    feat: improved display of create post

--- resources/views/pages/create-post.blade.php
+++ resources/views/pages/create-post.blade.php
@@ -1,34 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <main>
-        <h1>Create a New Post</h1>
-        <form action="{{ route('post.store') }}" method="POST" class="">
-            @csrf
-
-            @include('partials.input-field', [
-                'name' => 'title',
-                'label' => 'Post Title',
-                'placeholder' => 'Enter title',
-                'required' => true
-            ])
-
-            @include('partials.input-field', [
-                'name' => 'text',
-                'label' => 'Post Content',
-                'placeholder' => 'Write your post here...',
-                'required' => true
-            ])
-
-            <div class="form-check mb-3">
-                <input type="checkbox" id="is_public" name="is_public" class="form-check-input" value="1" checked>
-                <label for="is_public" class="form-check-label">Make this post public</label>
-                @error('is_public')
-                    <small class="text-danger">{{ $message }}</small>
-                @enderror
-            </div>
-
-            @include('partials.text-button', ['text' => 'Create Post', 'label' => 'create', 'type' => 'primary', 'submit' => true])
-        </form>
+    <main id="create-post-page" class="grid grid-cols-3 items-center">
+        <article class="card h-min p-10 pt-16 grid gap-12 justify-items-center col-start-2">
+            <h1 class="text-xl font-bold">Create a New Post</h1>
+            <form action="{{ route('post.store') }}" method="POST" class="grid gap-4 justify-self-stretch">
+                @csrf
+                
+                @include('partials.input-field', [
+                    'name' => 'title',
+                    'label' => 'Post Title',
+                    'placeholder' => 'Enter title',
+                    'required' => true
+                ])
+                
+                @include('partials.input-field', [
+                    'name' => 'text',
+                    'label' => 'Post Content',
+                    'placeholder' => 'Write your post here...',
+                    'required' => true
+                    ])
+                    
+                <div class="flex flex-col">
+                    <label class="mb-2">
+                        <input type="checkbox" name="is_public" value="1" checked> <span class="font-medium">Make this post public</span>
+                    </label>
+                </div>
+                            
+                @include('partials.text-button', ['text' => 'Create Post', 'label' => 'create', 'type' => 'primary', 'submit' => true])
+            </form>
+        </article>
     </main>
 @endsection
+    
