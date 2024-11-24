b1c9fe7a398481f932ee6eb0f95de4bbfeff75b5
racoelhosilva <racoelhosilva@gmail.com>
2024-11-24 23:11:30 +0000

    feat: improved timeline for authenticated users

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Follow;
 use App\Models\Post;
 use App\Models\Tag;
 use App\Models\User;
@@ -13,7 +14,15 @@ class HomeController extends Controller
     {
         return view('pages.home', [
             'users' => User::orderBy('num_followers', 'DESC')->limit(5)->get(),
-            'posts' => Post::with(['author', 'tags'])->orderBy('likes', 'DESC')->get(),   // with used to prevent the N+1 problem (see https://laravel-news.com/laravel-n1-query-problems for more details)
+            'posts' => Post::with(['author', 'tags'])
+                ->when(auth()->check(), function ($query) {
+                    $followedUserIds = Follow::where('follower_id', auth()->id())->pluck('followed_id');
+                    $query->orderByRaw('CASE WHEN author_id IN ('.$followedUserIds->join(',').') THEN 1 ELSE 2 END')->orderBy('likes', 'DESC');
+                }, function ($query) {
+                    $query->orderBy('likes', 'DESC');
+                })
+                ->get(),
+            // with used to prevent the N+1 problem (see https://laravel-news.com/laravel-n1-query-problems for more details)
             'tags' => Tag::withCount('posts')->orderBy('posts_count', 'DESC')->limit(10)->get(),
         ]);
     }
