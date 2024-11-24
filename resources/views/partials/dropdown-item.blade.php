02211682d6b7df0c4e1da75dad18fe84f6d387db
Process-ing <rbruno.p.o@gmail.com>
2024-11-24 15:02:06 +0000

    Adjust dropdown item padding

--- resources/views/partials/dropdown-item.blade.php
+++ resources/views/partials/dropdown-item.blade.php
@@ -1,12 +1,12 @@
 @props(['id', 'icon', 'text', 'anchorUrl'])
 
 @if (isset($anchorUrl))
-    <a href="{{ $anchorUrl }}" {{ isset($id) ? "id=$id" : '' }} class="px-4 py-2 hover:bg-slate-300 hover:dark:bg-slate-600 transition-colors font-medium flex">
+    <a href="{{ $anchorUrl }}" {{ isset($id) ? "id=$id" : '' }} class="px-4 py-2.5 hover:bg-slate-300 hover:dark:bg-slate-600 transition-colors font-medium flex">
         @include('partials.icon', ['name' => $icon])
         <span class="ps-2">{{ $text }}</span>
     </a>
 @else
-    <button {{ isset($id) ? "id=$id" : '' }} class="px-4 py-2 hover:bg-slate-300 hover:dark:bg-slate-600 transition-colors font-medium flex">
+    <button {{ isset($id) ? "id=$id" : '' }} class="px-4 py-2.5 hover:bg-slate-300 hover:dark:bg-slate-600 transition-colors font-medium flex">
         @include('partials.icon', ['name' => $icon])
         <span class="ps-2">{{ $text }}</span>
     </button>
