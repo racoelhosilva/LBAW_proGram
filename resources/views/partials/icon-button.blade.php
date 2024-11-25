f00725d8a7993729daf568b2d52291391d89d7e3
Process-ing <rbruno.p.o@gmail.com>
2024-11-25 09:21:55 +0000

    Refactor user management actions

--- resources/views/partials/icon-button.blade.php
+++ resources/views/partials/icon-button.blade.php
@@ -1,4 +1,4 @@
-@props(['iconName', 'id', 'label', 'type' => 'primary', 'anchorUrl', 'submit' => false])
+@props(['iconName', 'id', 'label', 'type' => 'primary', 'anchorUrl', 'submit' => false, 'class' => ''])
 
 @php
     $buttonClass = match($type) {
@@ -9,11 +9,11 @@
 @endphp
 
 @if (!isset($anchorUrl))
-    <button {{ isset($id) ? "id=$id" : "" }} {{ $submit ? 'type="submit"' : ''}} aria-label="{{ $label }}" class="p-3 {{ $buttonClass }}">
+    <button {{ isset($id) ? "id=$id" : "" }} {{ $submit ? 'type="submit"' : ''}} aria-label="{{ $label }}" class="p-3 {{ $buttonClass }} {{ $class }}">
         @include('partials.icon', ['name' => $iconName])
     </button>
 @else
-    <a href={{ $anchorUrl }} {{ isset($id) ? "id=$id" : "" }} aria-label="{{ $label }}" class="block p-3 {{ $buttonClass }}">
+    <a href={{ $anchorUrl }} {{ isset($id) ? "id=$id" : "" }} aria-label="{{ $label }}" class="p-3 {{ $buttonClass }} {{ $class }}">
         @include('partials.icon', ['name' => $iconName])
     </a>
 @endif
--- resources/views/partials/icon.blade.php
+++ resources/views/partials/icon.blade.php
@@ -52,4 +52,7 @@
     @case('user-round-x')
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round-x"><path d="M2 21a8 8 0 0 1 11.873-7"/><circle cx="10" cy="8" r="5"/><path d="m17 17 5 5"/><path d="m22 17-5 5"/></svg>
         @break
+    @case('x')
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+        @break
 @endswitch
