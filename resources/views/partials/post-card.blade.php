6846b3418b66d3213fadee5a3006ca47bf31b91b
Process-ing <rbruno.p.o@gmail.com>
2024-11-25 17:29:00 +0000

    Make post like button appear filled when post is liked

--- resources/views/partials/post-card.blade.php
+++ resources/views/partials/post-card.blade.php
@@ -5,7 +5,7 @@
     $postUrl = url('post/' . $post->id);
 @endphp
 
-<article class="card px-6 w-full grid grid-cols-[auto_1fr_auto] items-center content-start">
+<article class="card px-6 w-full grid grid-cols-[auto_1fr_auto] items-center content-start" data-post-id="{{ $post->id }}">
     <a href="{{ $authorUrl }}">
         <img src="{{ url('img/placeholder.png') }}" alt="{{ $post->author->name }}" class="w-12 h-12 rounded-full object-cover">
     </a>
@@ -31,7 +31,11 @@
         <p class="whitespace-pre-wrap">{{ str_replace("\\n", "\n", $post->text) }}</p>
     </div>
     <div class="-ms-3 col-span-3 grid grid-cols-[auto_auto_auto_1fr_50%] items-center">
-        @include('partials.icon-button', ['iconName' => 'heart', 'label' => 'Like', 'type' => 'transparent'])
+        @if ($post->likedBy(Auth::user()))
+            @include('partials.icon-button', ['iconName' => 'filled-heart', 'label' => 'Like', 'type' => 'transparent', 'class' => 'like-button liked'])
+        @else
+            @include('partials.icon-button', ['iconName' => 'heart', 'label' => 'Like', 'type' => 'transparent', 'class' => 'like-button'])
+        @endif
         <p class="me-3 font-medium select-none">{{ $post->likes }}</p>
         @include('partials.icon-button', ['iconName' => 'message-square-text', 'label' => 'Comments', 'type' => 'transparent', 'anchorUrl' => $postUrl])
         <p class="font-medium select-none">{{ $post->comments }}</p>
