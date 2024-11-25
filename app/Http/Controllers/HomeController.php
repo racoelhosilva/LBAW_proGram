405e0e4ef49bf340af0b88480c999a59e7716fe7
racoelhosilva <racoelhosilva@gmail.com>
2024-11-25 16:52:41 +0000

    feat: added decay for the homepage queries

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,9 +17,15 @@ class HomeController extends Controller
             'posts' => Post::with(['author', 'tags'])
                 ->when(auth()->check(), function ($query) {
                     $followedUserIds = Follow::where('follower_id', auth()->id())->pluck('followed_id');
-                    $query->where('author_id', '!=', auth()->id())->orderByRaw('CASE WHEN author_id IN ('.$followedUserIds->join(',').') THEN 1 ELSE 2 END')->orderBy('likes', 'DESC');
+
+                    $query->where('author_id', '!=', auth()->id())
+                        ->orderByRaw('CASE WHEN author_id IN ('.$followedUserIds->join(',').') THEN 1 ELSE 2 END')
+                        ->orderByRaw('(likes / POW((EXTRACT(EPOCH FROM (NOW() - creation_timestamp)) / 3600) + 2, 1.5)) DESC');
+                    // we used the formula: likes / (time_since_post + constant) ^ decay
+                    // as a decay formula to order posts on the homepage so that posts that are more recent
+                    // and have a higher number of likes are considered more popular and are shown first
                 }, function ($query) {
-                    $query->orderBy('likes', 'DESC');
+                    $query->orderByRaw('(likes / POW((EXTRACT(EPOCH FROM (NOW() - creation_timestamp)) / 3600) + 2, 1.5)) DESC');
                 })
                 ->get(),
             // with used to prevent the N+1 problem (see https://laravel-news.com/laravel-n1-query-problems for more details)
