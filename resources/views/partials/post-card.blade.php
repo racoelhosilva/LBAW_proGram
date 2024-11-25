f4c7d6cb2d54ad745006e075839e01433d1cd1ef
Process-ing <rbruno.p.o@gmail.com>
2024-11-25 15:58:47 +0000

    Refactor post actions

--- resources/views/partials/post-card.blade.php
+++ resources/views/partials/post-card.blade.php
@@ -14,16 +14,23 @@
         <p class="text-xs/3 pt-1 font-medium text-gray-500 dark:text-gray-400 select-none"><a href="{{ $authorUrl }}">{{ '@' . $post->author->handle }}</a>{{ ' • ' . $post->creation_timestamp->diffForHumans() }}</p>
     </div>
     <div class="ms-4 -me-3 flex">
-        @if ($post->author->id === auth()->id())
-            @include('partials.icon-button', ['iconName' => 'pencil', 'label' => 'Edit', 'type' => 'transparent', 'anchorUrl' => url('post/' . $post->id . '/edit'), 'submit' => false])
-        @endif
-        @include('partials.icon-button', ['iconName' => 'ellipsis', 'label' => 'Options', 'type' => 'transparent'])
+        <div class="dropdown">
+            @include('partials.icon-button', ['iconName' => 'ellipsis', 'label' => 'Options', 'type' => 'transparent'])
+            <div class="hidden">
+                <div>
+                    @include('partials.dropdown-item', ['icon' => 'message-circle', 'text' => 'See Post', 'anchorUrl' => url('post/' . $post->id)])
+                    @if ($post->author->id === auth()->id())
+                        @include('partials.dropdown-item', ['icon' => 'pencil', 'text' => 'Edit Post', 'anchorUrl' => route('post.edit', $post->id)])
+                    @endif
+                </div>
+            </div>
+        </div>
     </div>
     <div class="mt-4 col-span-3">
         <h1 class="font-bold text-xl"><a href="{{ $postUrl }}">{{ $post->title }}</a></h1>
         <p class="whitespace-pre-wrap">{{ str_replace("\\n", "\n", $post->text) }}</p>
     </div>
-    <div class="-ms-3 col-span-2 grid grid-cols-[auto_auto_auto_1fr_50%] items-center">
+    <div class="-ms-3 col-span-3 grid grid-cols-[auto_auto_auto_1fr_50%] items-center">
         @include('partials.icon-button', ['iconName' => 'heart', 'label' => 'Like', 'type' => 'transparent'])
         <p class="me-3 font-medium select-none">{{ $post->likes }}</p>
         @include('partials.icon-button', ['iconName' => 'message-square-text', 'label' => 'Comments', 'type' => 'transparent', 'anchorUrl' => $postUrl])
@@ -34,14 +41,5 @@
                 <span class="text-sm font-medium text-blue-600 dark:text-blue-400">{{ '#' . $tag->name }}</span>
             @endforeach
         </div>
-    </div>
-    <div class="ms-4 -ms-3 flex justify-end">
-        @if ($post->author->id === auth()->id())
-            <form method="post" action="{{ url('post/' . $post->id) }}">
-                @csrf
-                @method('DELETE')
-                @include('partials.icon-button', ['iconName' => 'trash', 'label' => 'Delete', 'type' => 'transparent', 'submit' => true])
-            </form>
-        @endif
     </div>
 </article>
