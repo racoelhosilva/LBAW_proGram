d061e0835084de84b9b748f845f03b418f6606b6
racoelhosilva <racoelhosilva@gmail.com>
2024-11-26 12:43:27 +0000

    feat: improve design of error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,8 +1,12 @@
-@extends('layouts.auth')
+@extends('layouts.app')
 
 @section('content')
     <main id="404-page" class="grid grid-cols-3 items-center">
-        @include('partials.error-page', ['error' => '404', 'message' => 'Page Not Found'])
+        @include('partials.error-page', [
+            'number' => '404', 
+            'error' => 'Oops! Page Not Found...', 
+            'message' => 'The page you are looking for might have been removed, renamed or is temporarily unavailable.',
+            ])
     </main> 
 @endsection
 
--- resources/views/partials/error-page.blade.php
+++ resources/views/partials/error-page.blade.php
@@ -1,8 +1,14 @@
 @props(['error', 'message'])
 
-<article class="h-min p-10 pt-16 grid gap-12 justify-items-center col-start-2">
-    @include('partials.logo', ['size' => 'large'])
-    <h1 class="text-3xl font-bold text-center">
-        {{ $error . ' - ' . $message }}
+<article class="h-min p-10 pt-16 grid gap-6 justify-items-center col-start-2">
+    <h1 class="text-9xl font-bold text-center">
+        {{ $number }}
     </h1>
+    <h2 class="text-3xl font-bold text-center text-blue-600 dark:text-blue-400">
+        {{ $error }}
+    </h2>
+    <h3 class="text-xl text-center">
+        {{ $message }}
+    </h3>
+    @include('partials.text-button', ['anchorUrl' => route('home'), 'text' => 'Return to Home Page'])
 </article>
