0d971f6470466c873a945044f98217af3afecc30
racoelhosilva <racoelhosilva@gmail.com>
2024-11-26 11:21:37 +0000

    feat: minor function refactor

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,9 +12,7 @@ class UserController extends Controller
         $user = User::findOrFail($id);
 
         $authuser = Auth::user();
-
         $isOwnProfile = $authuser && $authuser->id === $user->id;
-
         $isFollowing = $authuser && $authuser->following()->where('followed_id', $user->id)->exists();
 
         return view('pages.user', [
@@ -29,7 +27,7 @@ class UserController extends Controller
     {
         if ($currentUser->id !== $visitedUser->id) {
             // Case 1: If the IDs are different, focus on visitedUser's follows (user is not in its profile)
-            $users = $visitedUser->followers()
+            return $visitedUser->followers()
                 ->whereNotIn('follower_id', $currentUser->following()->pluck('followed_id')->toArray())
                 ->where('is_public', true)
                 ->orderBy('num_followers', 'desc')
@@ -37,13 +35,11 @@ class UserController extends Controller
                 ->get();
         } else {
             // Case 2: If the IDs are the same, focus on currentUser's follows (user is in its profile)
-            $users = User::whereNotIn('id', $currentUser->following()->pluck('followed_id')->toArray())
+            return User::whereNotIn('id', $currentUser->following()->pluck('followed_id')->toArray())
                 ->where('is_public', true)
                 ->orderBy('num_followers', 'desc')
                 ->take(5)
                 ->get();
         }
-
-        return $users;
     }
 }
