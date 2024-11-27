dc0c60d3fd594fee928a0ba7ba2caa42112d4c9f
racoelhosilva <racoelhosilva@gmail.com>
2024-11-27 09:21:49 +0000

    feat: post pages checked

--- resources/views/pages/edit-post.blade.php
+++ resources/views/pages/edit-post.blade.php
@@ -1,52 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-    <main id="create-post-page" class="grid grid-cols-3 items-center">
-        <article class="card h-min p-10 pt-16 grid justify-items-center col-start-2">
-            <h1 class="mb-12 text-xl font-bold">Edit Post</h1>
-            <form action="{{ route('post.update', $post->id) }}" method="POST" class="mb-4 grid gap-4 justify-self-stretch">
-                @csrf
+<main id="create-post-page" class="grid grid-cols-3 items-center">
+    <article class="card h-min p-10 pt-16 grid justify-items-center col-start-2">
+        <h1 class="mb-12 text-xl font-bold">Edit Post</h1>
+        <form action="{{ route('post.update', $post->id) }}" method="POST" class="mb-4 grid gap-4 justify-self-stretch">
+            @csrf
 
-                @method('PUT')
+            @method('PUT')
+            
+            @include('partials.input-field', [
+                'name' => 'title',
+                'label' => 'Post Title',
+                'placeholder' => 'Enter title',
+                'value' => $post->title,
+                'required' => true
+            ])
+            
+            @include('partials.textarea', [
+                'name' => 'text',
+                'label' => 'Post Content',
+                'placeholder' => 'Write your post here...',
+                'value' => $post->text,
+                'required' => true
+            ])
                 
-                @include('partials.input-field', [
-                    'name' => 'title',
-                    'label' => 'Post Title',
-                    'placeholder' => 'Enter title',
-                    'value' => $post->title,
-                    'required' => true
-                ])
-                
-                @include('partials.textarea', [
-                    'name' => 'text',
-                    'label' => 'Post Content',
-                    'placeholder' => 'Write your post here...',
-                    'value' => $post->text,
-                    'required' => true
-                ])
-                    
-                <div class="flex flex-col">
-                    <label for="tags" class="mb-2 font-medium">Associated Tags</label>
-                    <select name="tags[]" id="tags" multiple class="card overflow-auto">
-                        @foreach ($tags as $tag)
-                            <option {{ $post->hasTag($tag) ? "checked" : "" }} class="w-full text-gray-600 dark:text-white px-4 py-2" value="{{ $tag->id }}" {{ $post->hasTag($tag) ? "selected" : "" }}>{{ $tag->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
+            <div class="flex flex-col">
+                <label for="tags" class="mb-2 font-medium">Associated Tags</label>
+                <select name="tags[]" id="tags" multiple class="card overflow-auto">
+                    @foreach ($tags as $tag)
+                        <option {{ $post->hasTag($tag) ? "checked" : "" }} class="w-full text-gray-600 dark:text-white px-4 py-2" value="{{ $tag->id }}" {{ $post->hasTag($tag) ? "selected" : "" }}>{{ $tag->name }}</option>
+                    @endforeach
+                </select>
+            </div>
 
-                <div class="flex flex-col">
-                    <label class="mb-2">
-                        <input type="checkbox" name="is_public" value="1" {{ $post->is_public ? 'checked' : '' }}> 
-                        <span class="font-medium">Make this post public</span>
-                    </label>
-                </div>
-                @include('partials.text-button', ['text' => 'Update Post', 'label' => 'update', 'type' => 'primary', 'submit' => true])
-            </form>
-            <form method="post" action="{{ route('post.destroy', $post->id) }}" class="w-full flex flex-col">
-                @csrf
-                @method('DELETE')
-                @include('partials.text-button', ['text' => 'Delete Post', 'type' => 'primary', 'submit' => true])
-            </form>
-        </article>
-    </main>
+            <div class="flex flex-col">
+                <label class="mb-2">
+                    <input type="checkbox" name="is_public" value="1" {{ $post->is_public ? 'checked' : '' }}> 
+                    <span class="font-medium">Make this post public</span>
+                </label>
+            </div>
+            @include('partials.text-button', ['text' => 'Update Post', 'label' => 'update', 'type' => 'primary', 'submit' => true])
+        </form>
+        <form method="post" action="{{ route('post.destroy', $post->id) }}" class="w-full flex flex-col">
+            @csrf
+            @method('DELETE')
+            @include('partials.text-button', ['text' => 'Delete Post', 'type' => 'primary', 'submit' => true])
+        </form>
+    </article>
+</main>
 @endsection
--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -1,13 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-
 <main id="home-page" class="px-8 py-4 grid grid-cols-3 gap-6">
     <section id="post" class="col-span-2">
         @include('partials.post-card', ["post" => $post])
     </section>
     
-    <section class="card h-fit flex flex-col gap-3 col-span-1">
+    <section id="comment-section" class="card h-fit flex flex-col gap-3 col-span-1">
         <h1 class="text-xl font-bold">Comments</h1>
         @if ($post->comments > 0)
             <div class="flex-1 overflow-y-auto space-y-3">
@@ -20,5 +19,4 @@
         @endif
     </section>
 </main>
-
 @endsection
