8929b9ad21a4579ef77f6b9ce4c4c66d0a87569c
racoelhosilva <racoelhosilva@gmail.com>
2024-11-27 00:59:25 +0000

    feat: code polishing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,10 +15,7 @@ class UserController extends Controller
         $authuser = Auth::user();
         $isOwnProfile = $authuser && $authuser->id === $user->id;
         $isFollowing = $authuser && $authuser->following()->where('followed_id', $user->id)->exists();
-        $recommendedUsers = null;
-        if ($authuser !== null) {
-            $recommendedUsers = $this->recommendedUsers($authuser, $user);
-        }
+        $recommendedUsers = Auth::check() ? $this->recommendedUsers($authuser, $user) : null;
 
         return view('pages.user', [
             'user' => $user,
