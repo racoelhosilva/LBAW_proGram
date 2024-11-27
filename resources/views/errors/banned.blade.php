d1fd35d944f531cdd5b81e076b061d0efd4c6c1a
Process-ing <rbruno.p.o@gmail.com>
2024-11-27 17:25:46 +0000

    Add banned page

---
+++ resources/views/errors/banned.blade.php
@@ -0,0 +1,15 @@
+@props(['reason', 'expires'])
+
+@extends('layouts.app')
+
+@section('content')
+    <main id="banned-page" class="grid grid-cols-3 items-center">
+        @include('partials.error-page', [
+            'number' => '403', 
+            'error' => 'You are currently banned.',
+            'message' => "You do not have permission to access the site.\nReason: $reason\nExpires: $expires",
+            'button' => 'logout',
+            ])
+    </main> 
+@endsection
+
--- resources/views/errors/error-page.blade.php
+++ resources/views/errors/error-page.blade.php
@@ -1,4 +1,4 @@
-@props(['error', 'message'])
+@props(['number', 'error', 'message', 'button' => 'home'])
 
 <article class="h-min p-10 pt-16 grid justify-items-center col-start-2">
     <section id="error-info" class="py-12 grid gap-6 justify-items-center">
@@ -8,9 +8,17 @@
         <h2 class="text-3xl font-bold text-center text-blue-600 dark:text-blue-400">
             {{ $error }}
         </h2>
-        <h3 class="text-xl text-center">
-            {{ $message }}
-        </h3>
+        <h3 class="text-xl text-center whitespace-pre-wrap">{{ $message }}</h3>
     </section>
-    @include('partials.text-button', ['anchorUrl' => route('home'), 'text' => 'Return to Home Page'])
+    @switch ($button)
+        @case('home')
+            @include('partials.text-button', ['anchorUrl' => route('home'), 'text' => 'Return to Home Page'])
+            @break
+        @case('logout')
+            <form method="post" action="{{ route('logout') }}">
+                @csrf
+                @include('partials.text-button', ['text' => 'Logout', 'submit' => true])
+            </form>
+            @break
+    @endswitch
 </article>
