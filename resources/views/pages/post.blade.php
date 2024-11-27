3dac57450cdb131e01d03e8ad373b2193ca8830a
racoelhosilva <racoelhosilva@gmail.com>
2024-11-27 12:15:38 +0000

    feat: change to forelse

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -8,15 +8,13 @@
     
     <section id="comment-section" class="card h-fit flex flex-col gap-3 col-span-1">
         <h1 class="text-xl font-bold">Comments</h1>
-        @if ($post->comments > 0)
-            <div class="flex-1 overflow-y-auto space-y-3">
-                @foreach($post->allComments as $comment)
-                    @include('partials.comment-card', ['comment' => $comment])
-                @endforeach
-            </div>
-        @else
+        <div class="flex-1 overflow-y-auto space-y-3">
+        @forelse($post->allComments as $comment)
+            @include('partials.comment-card', ['comment' => $comment])
+        @empty
             <p>No comments on this post</p>
-        @endif
+        @endforelse
+        </div>
     </section>
 </main>
 @endsection
