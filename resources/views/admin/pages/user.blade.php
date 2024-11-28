2cc0ef02096290962594be1a26aa8a8309a140a2
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-11-28 00:24:20 +0000

    chore: changed url to route

--- resources/views/admin/pages/user.blade.php
+++ resources/views/admin/pages/user.blade.php
@@ -24,7 +24,6 @@
                         <td>{{ $user->handle }}</td>
                         <td>{{ $user->isBanned() ? 'Yes' : 'No' }}</td>
                         <td class="pe-8 flex justify-end gap-2">
-                            {{-- TODO: Place user profile --}}
                             @include('partials.text-button', ['text' => 'View Profile', 'type' => 'secondary', 'anchorUrl' => route('user.show', $user->id)])
                             <div class="modal ban-modal">
                                 @include('partials.text-button', ['text' => 'Ban', 'class' => 'open-button', 'type' => 'secondary'])
