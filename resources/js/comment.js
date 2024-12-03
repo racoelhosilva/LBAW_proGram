6ece9ff71ab7d1298b6b1f456be4efa297c0ad25
Jose Sousa <2409jmsousa@gmail.com>
2024-12-03 16:33:14 +0000

    chore: cleaned up code

--- resources/js/comment.js
+++ resources/js/comment.js
@@ -2,15 +2,12 @@ import { sendPost,sendPatch, sendToastMessage ,sendDelete} from './utils';
 
 const addSubmitCommentListener = () => {
     const form = document.getElementById('comment-submit-form');
-    console.log(form);
     form.addEventListener('submit', (event) => {
         event.preventDefault();
-        console.log('Submitting comment...');
         const formData = new FormData(form);
         const data = Object.fromEntries(formData.entries()); 
         data.post_id = parseInt(data.post_id,10);
         data.author_id = parseInt(data.author_id,10);
-        console.log(data);
         res = sendPost('/api/comment', data)
         .then((_) => {
             window.location.reload();
@@ -26,15 +23,12 @@ const addEditCommentListener = () => {
 
     const commentSection = document.getElementById('comment-section');
     if (!commentSection) return;
-    console.log("hi")
 
     const comments = commentSection.querySelectorAll('.comment-card');
-    console.log(comments)
     
     comments.forEach(comment => {
 
         const editButton = comment.querySelector('.edit-button-container button');
-        console.log(editButton)
         
         if (editButton) {
             editButton.addEventListener('click', () => {
@@ -43,24 +37,8 @@ const addEditCommentListener = () => {
                const text = paragraph.textContent;
                paragraph.remove();
                const textarea = document.createElement('textarea');
-               textarea.classList.add(
-                'w-full',
-                'px-5',
-                'py-3',
-                'rounded-lg',
-                'bg-white',
-                'dark:bg-slate-700',
-                'text-gray-600',
-                'dark:text-white',
-                'placeholder-gray-500',
-                'dark:placeholder-gray-400',
-                'border',
-                'border-slate-300',
-                'dark:border-slate-600',
-                'focus:border-blue-600',
-                'outline-none',
-                'resize-none',
-                'min-h-32'
+               textarea.classList.add(  
+                'edit-textarea'
             );
                textarea.textContent = text;
                contentContainer.appendChild(textarea);
@@ -70,13 +48,11 @@ const addEditCommentListener = () => {
                     'p-2',
                     'w-full',
                     'mt-2',
-                    'update-button'
-                    
+                    'update-button'    
                 )
                
                button.textContent = 'Save';
                button.addEventListener('click', async () => {
-                console.log('Updating comment...');
                 const updatedText = textarea.value;
                 const data ={}
                 data.content=updatedText;
@@ -111,7 +87,6 @@ const addDeleteCommentListener = () => {
         if (deleteButton) {
             deleteButton.addEventListener('click', () => {
                 const commentId = comment.dataset.commentId;
-                console.log('Deleting comment...');
                 sendDelete(`/api/comment/${commentId}`)
                     .then((_) => {
                         window.location.reload();
