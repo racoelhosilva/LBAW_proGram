6dbc1bab2646f1d33ba5b03c9f4ae63e9885cbdc
racoelhosilva <racoelhosilva@gmail.com>
2024-12-06 09:27:53 +0000

    feat: follower and following grid display

--- resources/views/pages/followers.blade.php
+++ resources/views/pages/followers.blade.php
@@ -5,14 +5,16 @@
 @section('content')
 <article id="users" class="mx-12 card space-y-3">
     <h3 class="text-xl font-bold">Followers of {{$user->name}}</h3>
-    @forelse ($user->followers as $follower)
-        @if (Auth::check() && Auth::id() == $user->id)
-            @include('partials.user-card', ['user' => $follower, 'buttonText' => 'Remove'])
-        @else
-            @include('partials.user-card', ['user' => $follower])
-        @endif
-    @empty
-        <p>This user has no followers</p>
-    @endforelse
+    <div class="grid gap-x-4 gap-y-2 grid-cols-3">
+        @forelse ($user->followers as $follower)
+            @if (Auth::check() && Auth::id() == $user->id)
+                @include('partials.user-card', ['user' => $follower, 'buttonText' => 'Remove'])
+            @else
+                @include('partials.user-card', ['user' => $follower])
+            @endif
+        @empty
+            <p>This user has no followers</p>
+        @endforelse
+    <div>
 </article>
 @endsection
--- resources/views/pages/following.blade.php
+++ resources/views/pages/following.blade.php
@@ -5,20 +5,20 @@
 @section('content')
 <article id="users" class="mx-12 card space-y-3">
     <h3 class="text-xl font-bold">Followed by {{$user->name}}</h3>
-    @forelse ($user->following as $following)
-
-        @if (Auth::check() && Auth::id() == $user->id)
-            <form action="{{ route('api.user.unfollow', $following->id) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                @include('partials.user-card', ['user' => $following, 'buttonText' => 'Unfollow'])
-            </form>
-
-        @else
-            @include('partials.user-card', ['user' => $following])
-        @endif
-    @empty
-        <p>This user does not follow any other user</p>
-    @endforelse
+    <div class="grid gap-x-4 gap-y-2 grid-cols-3">
+        @forelse ($user->following as $following)
+            @if (Auth::check() && Auth::id() == $user->id)
+                <form action="{{ route('api.user.unfollow', $following->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    @include('partials.user-card', ['user' => $following, 'buttonText' => 'Unfollow'])
+                </form>
+            @else
+                @include('partials.user-card', ['user' => $following])
+            @endif
+        @empty
+            <p>This user does not follow any other user</p>
+        @endforelse
+    </div>
 </article>
 @endsection
