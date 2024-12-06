9e3b22658d499b66e2de43917ea4a67cdbfd275c
Process-ing <rbruno.p.o@gmail.com>
2024-12-06 11:01:37 +0000

    Refactor search endpoint to separate types of search

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -1,3 +1,5 @@
+@props(['type', 'results'])
+
 @extends('layouts.app')
 @section('title') {{'Search | Program'}} @endsection
 @section('content')
@@ -5,37 +7,40 @@
         <section class="card h-min">
             <h1 class="pb-4 text-xl font-semibold">Search Options</h1>
             <div class="border-t border-slate-300 dark:border-slate-600 grid justify-stretch">
-                <button id="see-posts-button" class="px-4 py-2 hover:bg-slate-300 hover:dark:bg-slate-600 transition-colors font-bold flex">
+                <button id="see-posts-button" class="px-4 py-2 hover:bg-slate-300 hover:dark:bg-slate-600 transition-colors {{ $type === 'posts' ? 'font-bold' : 'font-medium' }} flex">
                     @include('partials.icon', ['name' => 'message-circle'])
                     <span class="ps-2">Posts</span>
                 </button>
-                <button id="see-users-button" class="px-4 py-2 hover:bg-slate-300 hover:dark:bg-slate-600 transition-colors font-medium flex">
+                <button id="see-users-button" class="px-4 py-2 hover:bg-slate-300 hover:dark:bg-slate-600 transition-colors {{ $type === 'users' ? 'font-bold' : 'font-medium' }} flex">
                     @include('partials.icon', ['name' => 'user-round'])
                     <span class="ps-2">Users</span>
                 </button>
             </div>
         </section>
         
-        <section id="post-results" class="flex flex-col col-span-3 gap-3">
-            @if (count($posts) > 0)
-                <h1 class="text-xl font-semibold">Found {{ count($posts) . (count($posts) === 1 ? ' post' : ' posts') }}</h1>
-                @foreach ($posts as $post)
-                    @include('partials.post-card', ['post' => $post])
-                @endforeach
-            @else
-                <h1 class="text-xl font-semibold">No posts found</h1>
-            @endif
-        </section>
-        
-        <section id="user-results" class="hidden flex-col col-span-3 gap-3">
-            @if (count($users) > 0)
-                <h1 class="text-xl font-semibold">Found {{ count($users) . (count($users) === 1 ? ' user' : ' users') }}</h1>
-                @foreach ($users as $user)
-                    @include('partials.user-card', ['user' => $user])
-                @endforeach
-            @else
-                <h1 class="text-xl font-semibold">No users found</h1>
-            @endif
+        <section id="results" class="flex flex-col col-span-3 gap-3">
+            @switch($type)
+                @case('posts')
+                    @if (count($results) > 0)
+                        <h1 class="text-xl font-semibold">Found {{ count($results) . (count($results) === 1 ? ' post' : ' posts') }}</h1>
+                        @foreach ($results as $post)
+                            @include('partials.post-card', ['post' => $post])
+                        @endforeach
+                        @break
+                    @else
+                        <h1 class="text-xl font-semibold">No posts found</h1>
+                    @endif
+                @case('users')
+                    @if (count($results) > 0)
+                        <h1 class="text-xl font-semibold">Found {{ count($results) . (count($results) === 1 ? ' user' : ' users') }}</h1>
+                        @foreach ($results as $user)
+                            @include('partials.user-card', ['user' => $user])
+                        @endforeach
+                        @break
+                    @else
+                        <h1 class="text-xl font-semibold">No users found</h1>
+                    @endif
+            @endswitch
         </section>
     </main>
 @endsection
