89ed57f34e70f9f8f6e90f1fd36fb94f78534578
Jose Sousa <2409jmsousa@gmail.com>
2024-12-07 00:53:46 +0000

    feat: initial contact us implementation

--- resources/views/pages/contactus.blade.php
+++ resources/views/pages/contactus.blade.php
@@ -7,18 +7,19 @@
         <section class="mb-8 text-center">
             <h1 class="text-4xl font-medium">Contact Us</h1>
         </section>
-        <section class="flex flex-col md:flex-row justify-center gap-8 items-start w-full m-20">
-            <article class="address card h-min p-4 border rounded-lg shadow">
+        <section class="flex flex-col  justify-center gap-8 items-center  w-full m-20 max-w-[50rem]">
+            <article class="address card  p-4 border rounded-lg shadow w-full">
                 <h2 class="text-2xl font-semibold mb-2">Address</h2>
-                <p>4200-465 Porto</p>
+                <p>R. Dr. Roberto Frias, 4200-465 Porto</p>
             </article>
-            <article class="phone card h-min p-4 border rounded-lg shadow">
+            <article class="phone card p-4 border rounded-lg shadow w-full">
                 <h2 class="text-2xl font-semibold mb-2">Phone</h2>
-                <p>+351 123 456 789</p>
+                <p>+351 22 508 1400
+                </p>
             </article>
-            <article class="email card h-min p-4 border rounded-lg shadow">
+            <article class="email card p-4 border rounded-lg shadow w-full">
                 <h2 class="text-2xl font-semibold mb-2">Email</h2>
-                <p>contact@program.com</p>
+                <a href="mailto:contact@program.com">contact@program.com</a>
             </article>
         </section>
     </main>
