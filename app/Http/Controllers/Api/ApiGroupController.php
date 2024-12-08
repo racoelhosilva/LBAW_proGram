a3574c552de72bf8127bc8750895241bf558ab89
Jose Sousa <2409jmsousa@gmail.com>
2024-12-08 12:59:39 +0000

    feat: public and private join logic is now different

--- app/Http/Controllers/Api/ApiGroupController.php
+++ app/Http/Controllers/Api/ApiGroupController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Group;
 use App\Models\GroupJoinRequest;
+use App\Models\GroupMember;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -15,11 +16,19 @@ class ApiGroupController extends Controller
         $group = Group::findOrFail($group_id);
         $this->authorize('join', $group);
         $user = Auth::user();
-        $groupJoinRequest = new GroupJoinRequest;
-        $groupJoinRequest->status = 'pending';
-        $groupJoinRequest->group_id = $group_id;
-        $groupJoinRequest->requester_id = $user->id;
-        $groupJoinRequest->save();
+        if ($group->is_public) {
+            $groupMember = new GroupMember;
+            $groupMember->group_id = $group_id;
+            $groupMember->user_id = $user->id;
+            $groupMember->join_timestamp = now();
+            $groupMember->save();
+        } else {
+            $groupJoinRequest = new GroupJoinRequest;
+            $groupJoinRequest->status = 'pending';
+            $groupJoinRequest->group_id = $group_id;
+            $groupJoinRequest->requester_id = $user->id;
+            $groupJoinRequest->save();
+        }
 
         return response()->json(['message' => 'You have joined the group.']);
     }
--- resources/views/pages/manage-group.blade.php
+++ resources/views/pages/manage-group.blade.php
@@ -4,25 +4,33 @@
 
 @section('content')
     <main id="manage-group-page" class="px-8 py-4 flex flex-col gap-6">
-        <header class="grid grid-cols-[auto_auto_auto] gap-0 w-full">
+        @if($group->is_public)
+            <header class="grid grid-cols-[auto_auto] gap-0 w-full">    
+        @else
+            <header class="grid grid-cols-[auto_auto_auto] gap-0 w-full">
+        @endif
             <a href="#members" 
                class="text-xl border text-center font-bold p-4 hover:border">Members</a>
-            <a href="#requests" 
-               class="text-xl border border-transparent text-center font-bold p-4  hover:border">Join Requests</a>
+            @if(!$group->is_public)
+                <a href="#requests" 
+                class="text-xl border border-transparent text-center font-bold p-4  hover:border">Join Requests</a>
+            @endif
             <a href="#invites" 
                class="text-xl border border-transparent text-center font-bold p-4 hover:border">Invites</a>
         </header>
         <section id="members">
             <h1>Members of {{$group->name}}</h1>
         </section>
-        <section id="requests" class="hidden">
-            <h1>Join Requests</h1>
-            @forelse ($usersWhoWantToJoin as $user)
-                @include('partials.user-card', ['user' => $user])
-            @empty
-                <p>No requests yet.</p>
-            @endforelse
-        </section>
+        @if(!$group->is_public)
+            <section id="requests" class="hidden">
+                <h1>Join Requests</h1>
+                @forelse ($usersWhoWantToJoin as $user)
+                    @include('partials.user-card', ['user' => $user])
+                @empty
+                    <p>No requests yet.</p>
+                @endforelse
+            </section>
+        @endif
         <section id="invites" class="hidden">
             <h1>Invites</h1>
         </section>
