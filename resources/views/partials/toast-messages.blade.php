402e34c2f302a72c63d8d2686c95de251720dea6
Process-ing <rbruno.p.o@gmail.com>
2024-12-10 17:49:57 +0000

    Add info toast message

--- resources/views/partials/toast-messages.blade.php
+++ resources/views/partials/toast-messages.blade.php
@@ -1,11 +1,16 @@
 <section id="toast-container" class="fixed bottom-4 right-4 space-y-4">
-    <article id="error-toast" class="toast-message {{ ! $errors->any() ? 'hidden' : '' }} p-4 rounded-lg bg-red-500 border-2 border-white text-white font-medium shadow-lg transition-opacity flex gap-3">
+    <article id="error-toast" class="toast-message {{ ! $errors->any() ? 'hidden' : '' }} p-4 rounded-lg bg-red-500 text-white font-medium shadow-2xl transition-opacity flex gap-3">
         @include('partials.icon', ['name' => 'circle-x'])
         <p>{{ $errors->first() }}</p>
     </article>
 
-    <article id="success-toast" class="toast-message {{ ! session('success') ? 'hidden' : '' }} p-4 rounded-lg bg-green-500 border-2 border-white text-white font-medium shadow-lg transition-opacity flex gap-3">
+    <article id="success-toast" class="toast-message {{ ! session('success') ? 'hidden' : '' }} p-4 rounded-lg bg-green-500 text-white font-medium shadow-2xl transition-opacity flex gap-3">
         @include('partials.icon', ['name' => 'circle-check'])
         <p>{{ session('success') }}</p>
+    </article>
+
+    <article id="info-toast" class="toast-message {{ ! session('info') ? 'hidden' : '' }} p-4 rounded-lg bg-blue-500 text-white font-medium shadow-2xl transition-opacity flex gap-3">
+        @include('partials.icon', ['name' => 'info'])
+        <p>{{ session('info') }}</p>
     </article>
 </section>
