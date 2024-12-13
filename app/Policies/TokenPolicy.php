8a44595b4dcfd79cbbe34f8140ceda86dfd2bf05
Process-ing <rbruno.p.o@gmail.com>
2024-12-13 11:03:01 +0000

    Create token policy

---
+++ app/Policies/TokenPolicy.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Token;
+use App\Models\User;
+
+class TokenPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(?User $user): bool
+    {
+        return $user && ! $user->isBanned();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(?User $user, Token $token): bool
+    {
+        return $user && ! $user->isBanned() && $user->id === $token->account->id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(?User $user): bool
+    {
+        return $user && ! $user->isBanned();
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(?User $user, Token $token): bool
+    {
+        return $user && ! $user->isBanned() && $user->id === $token->account->id;
+    }
+}
