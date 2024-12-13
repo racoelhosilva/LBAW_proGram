57c579838ee59adac875d00de2807f92cd02f928
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-12-13 12:27:56 +0000

    feat: edit profile form can change is_public

--- routes/web.php
+++ routes/web.php
@@ -85,7 +85,7 @@ Route::middleware('deny.banned')->group(function () {
     // User
     Route::controller(UserController::class)->group(function () {
         Route::get('/user/{id}', 'show')->where('id', '[0-9]+')->name('user.show');
-        Route::post('/user/{id}', 'update')->where('id', '[0-9]+')->name('user.update');
+        Route::put('/user/{id}', 'update')->where('id', '[0-9]+')->name('user.update');
         Route::get('/user/{id}/edit', 'edit')->where('id', '[0-9]+')->name('user.edit');
     });
 
