aefce27d721155d49e0bb9d41d7957d94b9137cd
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-12-16 15:12:25 +0000

    feat: decreased spacing between edit profile elements

--- resources/views/pages/edit-user.blade.php
+++ resources/views/pages/edit-user.blade.php
@@ -3,7 +3,7 @@
     {{ 'Edit ' . $user->name . ' | ProGram' }}
 @endsection
 @section('content')
-    <main class="card h-min p-10 pt-16 grid gap-12 justify-items-center col-start-2 m-6 md:m-16 lg:m-32">
+    <main class="card h-min p-10 pt-16 grid gap-6 justify-items-center col-start-2 m-6 md:m-16 lg:m-32">
         <h1 class="text-4xl font-bold">Edit Profile</h1>
         <form method="post" action="{{ route('user.update', $user->id) }}" class="grid gap-4 justify-self-stretch"
             id="profile-form" enctype="multipart/form-data">
