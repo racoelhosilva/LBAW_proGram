f16e50c7ccfb87b78e667a99bdb8e46be598fe50
Process-ing <rbruno.p.o@gmail.com>
2024-12-16 23:53:17 +0000

    Fix Quill editor overflow

--- resources/views/partials/quill-editor.blade.php
+++ resources/views/partials/quill-editor.blade.php
@@ -1,7 +1,9 @@
-@props(['name', 'label', 'value' => '', 'required' => false])
+@props(['name', 'label', 'value' => '', 'placeholder' => '', 'required' => false])
 
 <div>
     <label for="{{ $name }}" class="font-medium" @required($required)>{{ $label }}</label>
     <input type="hidden" name="{{ $name }}" value="{{ $value }}">
-    <div id="quill-editor"></div>
+    <div>
+        <div id="quill-editor"></div>  {{-- Double div to prevent Quill editor container overflow. --}}
+    </div>
 </div>
