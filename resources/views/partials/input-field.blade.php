883122185b740d03ca104a05cef71e61154dc6c0
Process-ing <rbruno.p.o@gmail.com>
2024-12-16 16:00:27 +0000

    Add arrow to tooltip

--- resources/views/partials/input-field.blade.php
+++ resources/views/partials/input-field.blade.php
@@ -7,6 +7,7 @@
         <p class="text-red-600 dark:text-red-400 text-sm font-medium">{{ $errors->first($name) }}</p>
     @endif
     <div class="tooltip px-4 py-2 rounded-md bg-gray-900 absolute top-3/4 left-1/2 z-10 mt-3 -translate-x-1/2 text-white opacity-0 group-hover:opacity-100 group-hover:top-full transition-all group-hover:delay-[2000ms] pointer-events-none">
+        <div class="w-4 h-4 bg-gray-900 absolute -top-1 left-1/2 -translate-x-1/2 rotate-45"></div>
         Tooltip
     </div>
 </div>
