59150a0cf0b11ddd8260df5c1caa7aa6614d0811
Process-ing <rbruno.p.o@gmail.com>
2024-12-17 19:12:58 +0000

    Add responsiveness to home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,13 +2,9 @@
 @section('title') {{'Home | ProGram'}} @endsection
 @section('content')
     <main id="home-page" class="px-8 grid grid-cols-4 gap-6">
-        <section class="card h-min flex flex-col gap-3">
+        <section class="card h-min hidden lg:flex flex-col gap-3 ">
             <h1 class="text-xl font-bold">Users On This Platform</h1>
-            @forelse ($users as $user)
-                @include('partials.user-card', ['user' => $user])
-            @empty
-                <p>No users at the moment</p>
-            @endforelse
+            @include('partials.user-list', ['users' => $users, 'responsive' => true])
 
             @if (!Auth::check())
                 <div class="flex justify-center pt-2">
@@ -20,7 +16,7 @@
             @endif
         </section>
 
-        <section class="card h-min space-y-3 col-span-2">
+        <section class="card h-min space-y-3 col-span-4 lg:col-span-2">
             <h1 class="text-xl font-bold">Recommended Posts</h1>
             <div id="home-posts" class="space-y-3">
                 @include('partials.post-list', ['posts' => $posts])
@@ -30,12 +26,12 @@
             </div>
         </section>
 
-        <section class="card h-min flex flex-col gap-3">
+        <section class="card h-min hidden lg:flex flex-col gap-3">
             <h1 class="text-xl font-bold">Trending Topics</h1>
             @forelse ($tags as $tag)
-                <div>
+                <div class="ms-4">
                     @include('partials.tag', ['tag' => $tag])
-                    <p class="ms-4 text-xs/3 text-gray-500 dark:text-gray-400 select-none">{{ $tag->posts->count() . ' posts' }}</p>
+                    <p class="text-xs/3 text-gray-500 dark:text-gray-400 select-none">{{ $tag->posts->count() . ' posts' }}</p>
                 </div>
             @empty
                 <p>No tags at the moment</p>
