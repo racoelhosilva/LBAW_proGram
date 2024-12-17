e2a987cdc1337fb9a1e760cf5d5983ddf21f76d9
Process-ing <rbruno.p.o@gmail.com>
2024-12-17 16:11:37 +0000

    Add alternative login buttons to register form

--- resources/views/auth/register-form.blade.php
+++ resources/views/auth/register-form.blade.php
@@ -22,6 +22,18 @@
                 <p class="text-sm text-green-500">{{ session('success') }}</p>
             @endif
         </div>
+
+        <div class="h-12 col-span-2 grid grid-cols-[1fr_auto_1fr] items-center gap-3">
+            <span class="h-px bg-gray-300 dark:bg-gray-700"></span>
+            Or Connect With
+            <span class="h-px bg-gray-300 dark:bg-gray-700"></span>
+        </div>
+        <div class="col-span-2 grid grid-cols-3 gap-4">
+            @include('auth.oauth-login-button', ['provider' => 'google'])
+            <!-- TODO: when the user is logging in using github after the first time, they get redirected automatically to the home page, but that process takes some time. maybe add a spinning loading indicator?  -->
+            @include('auth.oauth-login-button', ['provider' => 'github'])
+            @include('auth.oauth-login-button', ['provider' => 'gitlab'])
+        </div>
     </form>
 
     <p>
