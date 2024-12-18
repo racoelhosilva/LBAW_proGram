0a8fd6f978bb37880fd6c987372e384c5480d78d
Process-ing <rbruno.p.o@gmail.com>
2024-12-18 18:11:44 +0000

    Improve contact us page

--- resources/views/pages/contactus.blade.php
+++ resources/views/pages/contactus.blade.php
@@ -3,11 +3,9 @@
 @section('title', 'Contact Us | ProGram')
 
 @section('content')
-    <main id="contactus-page" class="min-h-[80vh] flex flex-col items-center justify-center px-4">
-        <section class="mb-8 text-center">
+    <main id="contactus-page" class="px-8 py-12 flex flex-col items-center justify-center">
+        <section class="flex flex-col justify-center gap-8 items-center w-full max-w-[40rem]">
             <h1 class="text-4xl font-medium">Contact Us</h1>
-        </section>
-        <section class="flex flex-col  justify-center gap-8 items-center  w-full m-20 max-w-[50rem]">
             <article class="address card  p-4 border rounded-lg shadow w-full">
                 <h2 class="text-2xl font-semibold mb-2">Address</h2>
                 <p>R. Dr. Roberto Frias, 4200-465 Porto</p>
