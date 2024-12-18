14729600305a505aaf376bccb1d89769b5f7b0f7
Process-ing <rbruno.p.o@gmail.com>
2024-12-18 23:08:36 +0000

    Make admin login responsive

--- resources/views/admin/auth/login-form.blade.php
+++ resources/views/admin/auth/login-form.blade.php
@@ -1,4 +1,4 @@
-<article class="card h-min p-10 pt-16 grid gap-12 justify-items-center col-start-2">
+<article class="card w-[40em] h-min p-10 pt-16 grid gap-12 justify-items-center">
     @include('admin.partials.logo', ['size' => 'large'])
     
     <form method="post" action="{{ route('admin.login') }}" class="grid gap-4 justify-self-stretch">
--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.auth')
 @section('title') {{'Admin Login | ProGram'}} @endsection
 @section('content')
-    <main id="admin-login-page" class="grid grid-cols-3 items-center">
+    <main id="admin-login-page" class="px-8 flex justify-center items-center">
         @include('admin.auth.login-form')
     </main>
 @endsection
