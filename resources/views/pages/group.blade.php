9aa1c202533349f4f3cbcd0a5e934e750988c92f
Jose Sousa <2409jmsousa@gmail.com>
2024-12-18 15:07:43 +0000

    Hide invites in public groups

--- resources/views/pages/group.blade.php
+++ resources/views/pages/group.blade.php
@@ -55,7 +55,9 @@
                                 <div>
                                     @include('partials.dropdown-item', ['icon' => 'user-round', 'text' => 'Manage Members', 'anchorUrl' => route('group.members', ['id' => $group->id])])
                                     @include('partials.dropdown-item', ['icon' => 'inbox', 'text' => 'Manage Requests', 'anchorUrl' => route('group.requests', ['id' => $group->id])])
-                                    @include('partials.dropdown-item', ['icon' => 'invite', 'text' => 'Manage Invites', 'anchorUrl' => route('group.invites', ['id' => $group->id])])
+                                    @if(!$group->is_public)
+                                        @include('partials.dropdown-item', ['icon' => 'invite', 'text' => 'Manage Invites', 'anchorUrl' => route('group.invites', ['id' => $group->id])])
+                                    @endif
                                 </div>
                             </div>
                         </article>
