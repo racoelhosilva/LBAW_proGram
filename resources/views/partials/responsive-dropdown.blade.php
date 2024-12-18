56397750dcf01fe0cc423f6e18ce0b64303cf2c9
Process-ing <rbruno.p.o@gmail.com>
2024-12-18 09:01:03 +0000

    Refactor responsive dropdown to allow contents appearing on bigger screens

--- resources/views/partials/responsive-dropdown.blade.php
+++ resources/views/partials/responsive-dropdown.blade.php
@@ -1,12 +1,12 @@
 @props(['label'])
 
-<div class="responsive-dropdown closed w-full h-auto">
-    <button class="h-12 px-4 font-medium rounded-lg hover:bg-slate-300 dark:hover:bg-slate-700 transition-colors flex gap-1 items-center">
+<div class="responsive-dropdown w-full h-auto col-span-4 lg:col-span-1">
+    <button class="h-12 px-4 font-medium rounded-lg hover:bg-slate-300 dark:hover:bg-slate-700 transition-colors flex lg:hidden gap-1 items-center">
         More Options
         @include('partials.icon', ['name' => 'chevron-down'])
         @include('partials.icon', ['name' => 'chevron-up'])
     </button>
-    <div class="flex flex-col gap-4">
+    <div class="max-lg:py-4 flex flex-col gap-4">
         <section id="search-options" class="card h-min flex flex-col">
             <h1 class="pb-4 text-xl font-semibold">Search Options</h1>
             <div class="grid justify-stretch">
