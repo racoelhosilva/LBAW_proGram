df1ea663940337150d0e3e4dc24dc3f4dfebabbc
Process-ing <rbruno.p.o@gmail.com>
2024-12-18 09:25:01 +0000

    Make login form responsive

--- resources/views/auth/login-form.blade.php
+++ resources/views/auth/login-form.blade.php
@@ -1,4 +1,4 @@
-<article class="card h-min p-10 pt-16 grid gap-12 justify-items-center col-start-2">
+<article class="card w-[40em] h-min p-10 pt-16 grid gap-12 justify-items-center">
     @include('partials.logo', ['size' => 'large'])
 
     <form method="post" action="{{ route('login') }}" class="grid gap-4 justify-self-stretch">
