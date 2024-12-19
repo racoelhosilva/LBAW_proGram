0fff9ecf6f874fa6500da653566f2dc7c9844f15
Process-ing <rbruno.p.o@gmail.com>
2024-12-19 01:47:42 +0000

    Make admin ban page responsive

--- resources/views/admin/pages/ban.blade.php
+++ resources/views/admin/pages/ban.blade.php
@@ -4,49 +4,59 @@
     <main class="px-4 flex flex-col gap-4">
         @include('admin.partials.search-field', ['route' => 'admin.ban.index'])
 
-        <table>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Username</th>
-                    <th>Admin</th>
-                    <th>Start</th>
-                    <th>Reason</th>
-                    <th>Duration</th>
-                    <th>Is Active?</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($bans as $ban)
+        <div class="overflow-x-auto">
+            <table>
+                <thead>
                     <tr>
-                        <td><strong>{{ $ban->id }}</strong></td>
-                        <td>{{ $ban->user->handle }}</td>
-                        <td>{{ $ban->administrator->name }}</td>
-                        <td>{{ $ban->start }}</td>
-                        <td>{{ $ban->reason }}</td>
-                        <td>{{ $ban->duration != '00:00:00' ? $ban->duration : 'Permanent' }}</td>
-                        <td>{{ $ban->isActive() ? 'Yes' : 'No' }}</td>
-                        <td class="pe-8 flex justify-end gap-2">
-                            @if ($ban->isActive())
-                                <form action="{{ route('admin.ban.revoke', $ban->id) }}" method="POST">
-                                    @csrf
-                                    @include('partials.text-button', [
-                                        'text' => 'Revoke',
-                                        'type' => 'secondary',
-                                        'submit' => true,
-                                    ])
-                                </form>
-                            @endif
-                        </td>
+                        <th>Id</th>
+                        <th>Username</th>
+                        <th>Admin</th>
+                        <th>Start</th>
+                        <th>Reason</th>
+                        <th>Duration</th>
+                        <th>Is Active?</th>
+                        <th>Actions</th>
                     </tr>
-                @empty
-                    <tr>
-                        <td colspan="8">No bans found.</td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @forelse ($bans as $ban)
+                        <tr>
+                            <td><strong>{{ $ban->id }}</strong></td>
+                            <td>{{ $ban->user->handle }}</td>
+                            <td>{{ $ban->administrator->name }}</td>
+                            <td>{{ $ban->start }}</td>
+                            <td>{{ $ban->reason }}</td>
+                            <td>{{ $ban->duration != '00:00:00' ? $ban->duration : 'Permanent' }}</td>
+                            <td>{{ $ban->isActive() ? 'Yes' : 'No' }}</td>
+                            <td class="flex justify-end">
+                                @if ($ban->isActive())
+                                    <div class="dropdown">
+                                        @include('partials.icon-button', ['iconName' => 'ellipsis', 'label' => 'Options', 'type' => 'transparent'])
+                                        <div class="hidden">
+                                            <div>
+                                                <form action="{{ route('admin.ban.revoke', $ban->id) }}" method="POST" class="flex flex-col">
+                                                    @csrf
+                                                    @include('partials.dropdown-item', [
+                                                        'icon' => 'user-round-x',
+                                                        'text' => 'Revoke',
+                                                        'type' => 'secondary',
+                                                        'submit' => true,
+                                                    ])
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endif
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="8">No bans found.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
         {{ $bans->onEachSide(0)->links() }}
     </main>
 @endsection
