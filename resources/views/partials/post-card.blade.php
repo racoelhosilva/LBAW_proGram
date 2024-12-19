be006698cc5444524d4409b14cef88d82d563bc5
racoelhosilva <racoelhosilva@gmail.com>
2024-12-19 00:22:19 +0000

    feat: added better inner shadows

--- resources/views/partials/post-card.blade.php
+++ resources/views/partials/post-card.blade.php
@@ -5,7 +5,11 @@
     $postUrl = route('post.show', $post->id);
 @endphp
 
-<article class="post-card card px-6 w-full grid grid-cols-[auto_1fr_auto] items-center content-start"
+<article class="post-card card px-6 w-full grid grid-cols-[auto_1fr_auto] items-center content-start
+    @if($post->is_announcement)
+        shadow-[inset_0_0_4px_rgba(38,102,214,0.8)] dark:shadow-[inset_0_0_4px_rgba(38,102,214,0.8)]
+    @endif
+    "
     data-post-id="{{ $post->id }}">
     @if (!$post->author->is_deleted)
         <a href="{{ $authorUrl }}">
