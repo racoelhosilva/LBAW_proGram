ca0f826c896798867977331ce4a2d6c29a7ac493
Process-ing <rbruno.p.o@gmail.com>
2024-12-19 00:43:55 +0000

    Improve post page responsivity

--- resources/views/admin/pages/post.blade.php
+++ resources/views/admin/pages/post.blade.php
@@ -4,49 +4,51 @@
     <main class="px-4 flex flex-col gap-4">
         @include('admin.partials.search-field', ['route' => 'admin.post.index'])
 
-        <table>
-            <thead class="text-center">
-                <tr>
-                    <th>ID</th>
-                    <th>Author</th>
-                    <th>Title</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($posts as $post)
-                    <tr class="border-t border-white">
-                        <td>{{ $post->id }}</td>
-                        <td class="break-words">{{ $post->author->name }}</td>
-                        <td class="break-all">{{ $post->title }}</td>
-                        <td class="flex justify-end">
-                            <div class="dropdown">
-                                @include('partials.icon-button', ['iconName' => 'ellipsis', 'label' => 'Options', 'type' => 'transparent'])
-                                <div class="hidden">
-                                    @include('partials.dropdown-item', [
-                                        'icon' => 'message-circle',
-                                        'text' => 'View Post',
-                                        'anchorUrl' => route('post.show', $post->id),
-                                    ])
-                                    <form method="post" action="{{ route('post.destroy', $post->id) }}" class="flex flex-col">
-                                        @csrf
-                                        @method('DELETE')
-                                        @include('partials.dropdown-item', [
-                                            'icon' => 'message-circle-x',
-                                            'text' => 'Delete Post',
-                                        ])
-                                    </form>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                @empty
+        <div class="overflow-x-auto">
+            <table>
+                <thead class="text-center">
                     <tr>
-                        <td colspan="4">No posts found.</td>
+                        <th>ID</th>
+                        <th>Author</th>
+                        <th>Title</th>
+                        <th>Actions</th>
                     </tr>
-                @endforelse
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @forelse ($posts as $post)
+                        <tr class="border-t border-white">
+                            <td>{{ $post->id }}</td>
+                            <td>{{ $post->author->name }}</td>
+                            <td>{{ $post->title }}</td>
+                            <td class="flex justify-end">
+                                <div class="dropdown">
+                                    @include('partials.icon-button', ['iconName' => 'ellipsis', 'label' => 'Options', 'type' => 'transparent'])
+                                    <div class="hidden">
+                                        @include('partials.dropdown-item', [
+                                            'icon' => 'message-circle',
+                                            'text' => 'View Post',
+                                            'anchorUrl' => route('post.show', $post->id),
+                                        ])
+                                        <form method="post" action="{{ route('post.destroy', $post->id) }}" class="flex flex-col">
+                                            @csrf
+                                            @method('DELETE')
+                                            @include('partials.dropdown-item', [
+                                                'icon' => 'message-circle-x',
+                                                'text' => 'Delete Post',
+                                            ])
+                                        </form>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="4">No posts found.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
         {{ $posts->onEachSide(0)->links() }}
     </main>
 @endsection
