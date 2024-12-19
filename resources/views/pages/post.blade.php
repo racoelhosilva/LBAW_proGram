7da63a8f9779a9edac312349dd6cda134b0d17ef
racoelhosilva <racoelhosilva@gmail.com>
2024-12-19 01:18:28 +0000

    feat: removed form on the comment section

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -8,21 +8,23 @@
     
     <section id="comment-section" class="card h-fit flex flex-col gap-3 col-span-1">
         <h1 class="text-xl font-bold">Comments</h1>
-        <form id ="comment-submit-form" action="{{ route('api.comment.store') }}" >
-            @csrf
-            @include('partials.textarea', ['name' => 'content', 'placeholder' => 'Write a comment...', 'label' => ''])
-            <input type="hidden" name="post_id" value="{{ $post->id }}">
-            @if (Auth::check())
-                <input type="hidden" name="author_id" value="{{ auth()->id() }}">
-            @endif
-            @include('partials.text-button', ['text' => 'Post Comment', 'class' => 'w-full mt-2 mb-2', 'submit' => true])
-        </form>
+        @auth
+            <form id ="comment-submit-form" action="{{ route('api.comment.store') }}" >
+                @csrf
+                @include('partials.textarea', ['name' => 'content', 'placeholder' => 'Write a comment...', 'label' => ''])
+                <input type="hidden" name="post_id" value="{{ $post->id }}">
+                @if (Auth::check())
+                    <input type="hidden" name="author_id" value="{{ auth()->id() }}">
+                @endif
+                @include('partials.text-button', ['text' => 'Post Comment', 'class' => 'w-full mt-2 mb-2', 'submit' => true])
+            </form>
+        @endauth
         <div class="flex-1 overflow-y-auto space-y-3 comment-list">
-        @forelse($post->allComments as $comment)
-            @include('partials.comment-card', ['comment' => $comment])
-        @empty
-            <p>No comments on this post</p>
-        @endforelse
+            @forelse($post->allComments as $comment)
+                @include('partials.comment-card', ['comment' => $comment])
+            @empty
+                <p>No comments on this post</p>
+            @endforelse
         </div>
     </section>
 </main>
