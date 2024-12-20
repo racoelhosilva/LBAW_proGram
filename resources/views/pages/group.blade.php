a7019c3b4e3f91ed6be0899a7f9960dabb684eab
Process-ing <rbruno.p.o@gmail.com>
2024-12-20 19:29:18 +0000

    Add contextual help to group page

--- resources/views/pages/group.blade.php
+++ resources/views/pages/group.blade.php
@@ -9,9 +9,23 @@
                 <h1 class="text-4xl font-bold">{{ $group->name }}</h1>
                 <div class="min-w-[24px]">
                     @if($group->is_public)
-                        @include('partials.icon', ['name' => 'earth','type' => 'secondary'])
+                        <article class="help-icon group relative">
+                            <div class="rounded-md">
+                                @include('partials.icon', ['name' => 'earth'])
+                            </div>
+                            <div class="w-80 px-4 py-2 rounded-md bg-gray-900 shadow-lg absolute top-8 {{ $left ? 'right-full translate-x-full' : 'left-full -translate-x-full' }} z-30 mt-3 text-white opacity-0 group-hover:opacity-100 group-hover:top-6 transition-all pointer-events-none">
+                                Public
+                            </div>
+                        </article>
                     @else
-                        @include('partials.icon', ['name' => 'lock', 'type' => 'secondary'])
+                        <article class="help-icon group relative">
+                            <div class="rounded-md">
+                                @include('partials.icon', ['name' => 'lock'])
+                            </div>
+                            <div class="w-80 px-4 py-2 rounded-md bg-gray-900 shadow-lg absolute top-8 {{ $left ? 'right-full translate-x-full' : 'left-full -translate-x-full' }} z-30 mt-3 text-white opacity-0 group-hover:opacity-100 group-hover:top-6 transition-all pointer-events-none">
+                                Private
+                            </div>
+                        </article>
                     @endif
                 </div>
             </div>
