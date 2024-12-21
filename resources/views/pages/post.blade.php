2f565c06f878f289e46ec4a13fe2f015be902785
Process-ing <rbruno.p.o@gmail.com>
2024-12-21 15:45:03 +0000

    Fix invalid html on post page

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('title') {{'Post ' . $post->title . ' | ProGram'}} @endsection
 @section('content')
-<main id="home-page" class="px-8 py-4 grid grid-cols-3 grid-rows-[auto_1fr] gap-6">
-    <section id="post" class="col-span-3 lg:col-span-2">
+<main id="post-page" class="px-8 py-4 grid grid-cols-3 grid-rows-[auto_1fr] gap-6">
+    <div class="col-span-3 lg:col-span-2">
         @include('partials.post-card', ["post" => $post])
-    </section>
+    </div>
     
     <section id="comment-section" class="card flex flex-col gap-3 col-span-3 lg:col-span-1" style="height: calc(100vh - 6rem - 4rem - 3rem);">
         <h1 class="text-xl font-bold">Comments</h1>
--- resources/views/partials/comment-card.blade.php
+++ resources/views/partials/comment-card.blade.php
@@ -6,7 +6,7 @@
 
 <article class="comment-card card px-6 w-full grid grid-cols-[1fr_auto] gap-x-1 items-center content-start" data-comment-id="{{ $comment->id }}">
     <div class="flex flex-col">
-        <p class="text-base/4 font-medium"><a href="{{ $authorUrl }}">{{ $comment->author->name }}</a></p>
+        <h2 class="text-base/4 font-medium"><a href="{{ $authorUrl }}">{{ $comment->author->name }}</a></h2>
         <p class="text-xs/3 pt-1 font-medium text-gray-500 dark:text-gray-400"><a href="{{ $authorUrl }}">{{ '@' . $comment->author->handle }}</a>{{ ' • ' . $comment->timestamp->diffForHumans() }}</p>
     </div>
     <div class="flex">
