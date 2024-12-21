5c4e9cbd8e2f5a72707a767db054488d9e805d61
Process-ing <rbruno.p.o@gmail.com>
2024-12-21 20:54:16 +0000

    Add field to distinguish mandatory input

--- resources/views/partials/input-field.blade.php
+++ resources/views/partials/input-field.blade.php
@@ -1,7 +1,7 @@
 @props(['type' => 'text', 'name', 'label', 'value', 'placeholder', 'required' => false, 'help'])
 
 <div class="grid grid-cols-[1fr_auto] gap-x-2 items-center">
-    <label for="{{ $name }}" class="font-medium col-span-2">{{ $label }}</label>
+    <label for="{{ $name }}" class="font-medium col-span-2">{{ $label . ($required ? '*' : '') }}</label>
     <input id="{{ $name }}" type="{{ $type }}" name="{{ $name }}" placeholder="{{ $placeholder }}"
            value="{{ $value ?? '' }}"
            {{ $required ? 'required' : '' }} class="h-12 w-full px-5 rounded-lg font-medium disabled:text-gray-500 disabled:dark:text-gray-400 bg-white dark:bg-slate-700 text-gray-700 dark:text-white placeholder-gray-500 dark:placeholder-gray-400 border border-slate-300 dark:border-slate-600 focus:border-blue-600 outline-none {{ isset($help) ? 'col-span-1' : 'col-span-2' }}">
--- resources/views/partials/textarea.blade.php
+++ resources/views/partials/textarea.blade.php
@@ -1,7 +1,7 @@
 @props(['name', 'label', 'value' => '', 'placeholder' => '', 'required' => false, 'rows' => 4, 'help'])
 
 <div class="grid grid-cols-[1fr_auto] items-start gap-x-2">
-    <label for="{{ $name }}" class="font-medium col-span-2">{{ $label }}</label>
+    <label for="{{ $name }}" class="font-medium col-span-2">{{ $label . ($required ? '*' : '') }}</label>
     <textarea id="{{ $name }}" name="{{ $name }}" placeholder="{{ $placeholder }}"
               {{ $required ? 'required' : '' }} rows="{{ $rows }}"
               class="w-full px-5 py-3 rounded-lg bg-white dark:bg-slate-700 text-gray-600 dark:text-white placeholder-gray-500 dark:placeholder-gray-400 border border-slate-300 dark:border-slate-600 focus:border-blue-600 outline-none resize-none">{{ old($name, $value) }}</textarea>
