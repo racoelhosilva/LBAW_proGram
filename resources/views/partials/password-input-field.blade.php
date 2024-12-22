637b2d340b0128a2d8c3fb40e249c44ef4491d8a
Process-ing <rbruno.p.o@gmail.com>
2024-12-22 13:23:18 +0000

    Fix password input on register form

--- resources/views/partials/password-input-field.blade.php
+++ resources/views/partials/password-input-field.blade.php
@@ -1,9 +1,9 @@
-@props(['type' => 'text', 'name', 'label', 'value', 'placeholder', 'required' => false])
+@props(['name', 'label', 'value', 'placeholder', 'required' => false])
 
 <div>
     <label for="{{ $name }}" class="font-medium">{{ $label . ($required ? '*' : '') }}</label>
     <div class="relative">
-        <input id="{{ $name }}" type="{{ $type }}" name="{{ $name }}"
+        <input id="{{ $name }}" type="password" name="{{ $name }}"
             placeholder="{{ $placeholder }}" value="{{ $value ?? '' }}" pattern="^.{,8}" @required($required)
             class="password-input h-12 w-full px-5 rounded-lg font-medium disabled:text-gray-500 disabled:dark:text-gray-400 bg-white dark:bg-slate-700 text-gray-700 dark:text-white placeholder-gray-500 dark:placeholder-gray-400 border border-slate-300 dark:border-slate-600 focus:border-blue-600 outline-none">
         <button type="button"
