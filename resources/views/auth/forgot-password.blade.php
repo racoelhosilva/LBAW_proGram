5480a5b77c44509e8210f8b080424f2ce18815c7
Process-ing <rbruno.p.o@gmail.com>
2024-12-22 00:37:13 +0000

    Refactor auth pages

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.auth')
-@section('title')
-    {{ 'Reset Password | ProGram' }}
-@endsection
+@section('title', 'Reset Password | ProGram')
 @section('content')
     <main id="reset-password-page" class="px-8 flex justify-center items-center">
         @include('auth.forgot-password-form')
